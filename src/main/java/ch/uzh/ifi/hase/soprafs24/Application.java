e408ca579048e6fcf1f0b86321f2e8a20f7fa853
Sakshi Chaudhari <112864544+DS-sakshi@users.noreply.github.com>
2025-04-10 19:32:46 +0200

    Update Application.java

--- src/main/java/ch/uzh/ifi/hase/soprafs24/Application.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/Application.java
@@ -8,7 +8,6 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @RestController
 @SpringBootApplication
 public class Application {
